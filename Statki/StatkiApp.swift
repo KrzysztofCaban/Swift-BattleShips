906119afa9675720fdff14099d271025979f2c9a
Krzysztof Caban <kcaban99@gmail.com>
2024-01-07 15:04:15 +0100

    Initial Commit

---
+++ Statki/StatkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatkiApp.swift
+//  Statki
+//
+//  Created by Krzysztof Caban on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StatkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
