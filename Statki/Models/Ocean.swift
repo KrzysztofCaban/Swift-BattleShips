13207495fc6876f6001c22d370143c1e743e0406
kacper <kacperadamiak14@gmail.com>
2024-01-07 18:40:53 +0100

    Added comments to Ocean.swift

--- Statki/Models/Ocean.swift
+++ Statki/Models/Ocean.swift
@@ -1,52 +1,63 @@
 import Foundation
 
 struct Ocean {
+    // Zmienne przechowujące liczbę kolumn i wierszy
     var numCols: Int
     var numRows: Int
+    // Prywatna zmienna przechowująca punkt początkowy (0,0)
     private var origin: Coordinate = .zero
     
+    // Inicjalizator struktury Ocean
     init(numCols: Int, numRows: Int) {
         self.numCols = numCols
         self.numRows = numRows
     }
     
-    //CHECK IF OCEAN CONTAINS THE COORDINATE POINT
+     // Funkcja sprawdzająca, czy ocean zawiera dany punkt
     func contains(_ point: Coordinate) ->Bool
     {
+        // Zwraca prawdę, jeśli punkt znajduje się w granicach oceanu
         return (point.x >= origin.x && point.y >= origin.y && point.x < numCols && point.y < numRows)
     }
     
-    //CHECK IF OCEAN CONTAINS ALL THE GIVEN COORDINATES
+    // Funkcja sprawdzająca, czy ocean zawiera wszystkie podane punkty
     func contains(_ points: Array<Coordinate>) ->Bool
     {
+        // Zwraca prawdę, jeśli żaden z punktów nie znajduje się poza granicami oceanu
         return !points.contains(where:{!self.contains($0)})
     }
     
-    //RETURN LOCATIONS THAT FIT WITH SHIP WITH GIVEN LENGTH
+    // Funkcja zwracająca lokalizacje, które pozwalają na umieszczenie statku o podanej długości
     func locationsThatFit(length: Int) -> [[Coordinate]] {
+        // Inicjalizacja tablicy przechowującej możliwe lokalizacje statków
         var locations = [[Coordinate]]()
         
-        //HORIZONTAL FITS
+        // Sprawdzenie dopasowań statku w poziomie
         for y in 0..<numRows {
             for start in 0...(numCols - length) {
                 var span = [Coordinate]()
                 for x in start..<(start + length) {
+                    // Dodanie współrzędnych do tablicy
                     span.append(Coordinate(x: x, y: y))
                 }
+                // Dodanie tablicy współrzędnych do tablicy możliwych lokalizacji
                 locations.append(span)
             }
         }
         
-        //VERTICAL FITS
+        // Sprawdzenie dopasowań statku w pionie
         for x in 0..<numCols {
             for start in 0...(numRows - length) {
                 var span = [Coordinate]()
                 for y in start..<(start + length) {
+                    // Dodanie współrzędnych do tablicy
                     span.append(Coordinate(x:x, y:y))
                 }
+                // Dodanie tablicy współrzędnych do tablicy możliwych lokalizacji
                 locations.append(span)
             }
         }
+        // Zwrócenie tablicy możliwych lokalizacji
         return locations
     }
 }
