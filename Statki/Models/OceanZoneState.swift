958fb7da2832024c13a5c11092a16b7e1f288b2d
kacper <kacperadamiak14@gmail.com>
2024-01-07 19:15:28 +0100

    Added comments to OceanZoneState.swift

--- Statki/Models/OceanZoneState.swift
+++ Statki/Models/OceanZoneState.swift
@@ -1,7 +1,13 @@
 import Foundation
 
+// Definiujemy typ wyliczeniowy (enum) o nazwie OceanZoneState. 
+// Ten typ wyliczeniowy reprezentuje różne stany, które może przyjąć strefa oceanu w kontekście gry w statki.
 enum OceanZoneState {
+    // Stan "clear" oznacza, że w strefie nie został wykonany strzał. 
+    // Może zawierać statek (Ship?), ale nie musi - stąd opcjonalny typ Ship.
     case clear(Ship?)
+    // Stan "miss" oznacza, że w tej strefie wykonano strzał, ale nie trafił on w żaden statek.
     case miss
+    // Stan "hit" oznacza, że w tej strefie wykonano strzał, który trafił w statek.
     case hit
 }
