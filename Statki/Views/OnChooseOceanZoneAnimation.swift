a45896576ac2db8d17904ff0b9f24f60f685717d
Krzysztof Caban <kcaban99@gmail.com>
2024-01-08 20:46:00 +0100

    Kacper's file found - Kacper's Atlantis

--- Statki/Views/OnChooseOceanZoneAnimation.swift
+++ Statki/Views/OnChooseOceanZoneAnimation.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct OnChooseOceanZoneAnimation: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    @Binding var zoneState: OceanZoneState
+    private let oceanSquare = CGSize(width: 0.95, height: 0.95)
+    @State private var isAnimating = false
+    @State private var triangleOffset: CGSize = CGSize(width: 0, height: 200)
+    @State private var triangleScale: CGFloat = 2.0
 
-#Preview {
-    OnChooseOceanZoneAnimation()
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                if isAnimating {
+                    switch zoneState {
+                    case .hit:
+                        ScaledShape(shape: Rectangle(), scale: oceanSquare)
+                            .fill(.gray)
+                            .opacity(0.6)
+                            .overlay(
+                                FireAnimation().frame(width: 50, height: 50)
+                            )
+                    default:
+                        ScaledShape(shape: Rectangle(), scale: oceanSquare)
+                            .fill(.blue)
+                            .opacity(0.8)
+                            .overlay(
+                                WaterAnimation().frame(width: 50, height: 50)
+                            )
+                    }
+
+                } else {
+                    RocketShape()
+                        .fill(Color.black)
+                        .frame(width: 50, height: 50)
+                        .scaleEffect(triangleScale)
+                        .offset(triangleOffset)
+                        .rotationEffect(.degrees(180))
+                        .onAppear {
+                            animateRocket(geometry: geometry)
+                        }
+                }
+            }
+        }
+    }
+
+    func animateRocket(geometry: GeometryProxy) {
+        withAnimation(Animation.easeInOut(duration: 0.5)) {
+            triangleOffset = CGSize(width: geometry.size.width / 2 - 25, height: geometry.size.height / 2 - 25)
+            triangleScale = 0.2
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            withAnimation {
+                isAnimating = true
+            }
+        }
+    }
 }
