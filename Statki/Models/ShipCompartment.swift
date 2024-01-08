36238492efe9d6a7c002aa203eaba7bba1c540ca
kacper <kacperadamiak14@gmail.com>
2024-01-08 10:41:05 +0100

    Added comments ShipCompartment.swift

--- Statki/Models/ShipCompartment.swift
+++ Statki/Models/ShipCompartment.swift
@@ -1,16 +1,20 @@
 import Foundation
 
-//REPRESENTS A SINGLE COMPARMENT WITHIN A SHIP
+// Klasa ShipCompartment reprezentuje pojedynczą część w statku
 class ShipCompartment {
+    // Właściwość przechowująca lokalizację części statku
     var location: Coordinate = .zero
+     // Właściwość przechowująca informację, czy część jest zalana
     var flooded: Bool = false
     
+    // Inicjalizator klasy ShipCompartment
     init(location: Coordinate, flooded:Bool = false) {
         self.location = location
         self.flooded = flooded
     }
 }
 
+// Rozszerzenie klasy ShipCompartment o protokół CustomStringConvertible
 extension ShipCompartment: CustomStringConvertible {
     var description: String {
         return location.description
